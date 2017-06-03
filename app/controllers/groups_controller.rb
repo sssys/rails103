727094f5bc38421abe4f08064a1b8b7da1bf1363
Wu <457492854@qq.com>
2017-06-03 11:16:05 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,12 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
 
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
   end
 
   def update
@@ -32,7 +35,7 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
     @group.destroy
     redirect_to groups_path, alert: "Group Delete "
-  end 
+  end
 
   private
 
