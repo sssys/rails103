1d4b90996ebd65321b1a0b53c92c5e22bcda578f
Wu <457492854@qq.com>
2017-06-09 11:41:16 +0800

    extract work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+  before_action :find_post_and_check_permission , :only => [:edit, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
@@ -19,10 +20,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post Delete"
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_post_and_check_permission
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to root_path, alert: "You have no permission"
+    end
   end
 
 end
